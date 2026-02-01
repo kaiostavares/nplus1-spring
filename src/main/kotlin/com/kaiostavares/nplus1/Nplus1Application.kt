2a7680df1eaf0b0cf2ec523551b8522db88d9c86
kaiostavares <kaio.stavaress@gmail.com>
2026-02-01 20:31:10 -0300

    init project

---
+++ src/main/kotlin/com/kaiostavares/nplus1/Nplus1Application.kt
@@ -0,0 +1,11 @@
+package com.kaiostavares.nplus1
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Nplus1Application
+
+fun main(args: Array<String>) {
+	runApplication<Nplus1Application>(*args)
+}
---
+++ src/test/kotlin/com/kaiostavares/nplus1/Nplus1ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kaiostavares.nplus1
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Nplus1ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
